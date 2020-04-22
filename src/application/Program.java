83c25a0eee041db92b7a4672979497ff14894a20
Luan Conte Soares <viperluan@gmail.com>
2020-04-22 01:48:05 -0300

    Little improvement in promotion printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,7 +43,13 @@ public class Program {
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
 					String type = sc.nextLine().toUpperCase();
-					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						UI.clearScreen();
+						UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+						System.out.println();
+						System.out.println("Target: " + target);
+
 						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
 						type = sc.nextLine().toUpperCase();
 					}
